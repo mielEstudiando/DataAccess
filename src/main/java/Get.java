c12ffaa80d39264b28f8c52df113ba8d8ff9d43c
Miel <m.ferrada09@ufromail.cl>
2024-06-19 10:46:11 -0400

    Agregado get para visor, funcional

--- src/main/java/Get.java
+++ src/main/java/Get.java
@@ -8,34 +8,39 @@ public class Get {
     }
 
     public void verLibros(String USER, String PASS, String DB_URL) {
-        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)) {
-            System.out.println("Conectando a la base de datos...");
-            System.out.println("Creando declaración...");
-            String sql = "SELECT * FROM Libro";
+        try {
+            // Cargar el driver JDBC de PostgreSQL
+            Class.forName("org.postgresql.Driver");
 
-            try (Statement stmt = conn.createStatement();
-                 ResultSet rs = stmt.executeQuery(sql)) {
+            try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS)) {
+                System.out.println("Conectando a la base de datos...");
+                System.out.println("Creando declaración...");
+                String sql = "SELECT * FROM Libro";
 
-                // Imprimir los nombres de las columnas
-                ResultSetMetaData metaData = rs.getMetaData();
-                int columnCount = metaData.getColumnCount();
-                for (int i = 1; i <= columnCount; i++) {
-                    System.out.print(metaData.getColumnName(i) + "\t");
-                }
-                System.out.println();
+                try (Statement stmt = conn.createStatement();
+                     ResultSet rs = stmt.executeQuery(sql)) {
 
-                // Imprimir los datos de cada fila
-                while (rs.next()) {
+                    // Imprimir los nombres de las columnas
+                    ResultSetMetaData metaData = rs.getMetaData();
+                    int columnCount = metaData.getColumnCount();
                     for (int i = 1; i <= columnCount; i++) {
-                        System.out.print(rs.getString(i) + "\t");
+                        System.out.print(metaData.getColumnName(i) + "\t");
                     }
                     System.out.println();
-                }
-            }
 
-        } catch (SQLException se) {
-            se.printStackTrace();
-        } catch (Exception e) {
+                    // Imprimir los datos de cada fila
+                    while (rs.next()) {
+                        for (int i = 1; i <= columnCount; i++) {
+                            System.out.print(rs.getString(i) + "\t");
+                        }
+                        System.out.println();
+                    }
+                }
+
+            } catch (SQLException se) {
+                se.printStackTrace();
+            }
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("melo es guatón");
+
+        String url = "jdbc:postgresql://localhost:5432/prueba";
+        String user = "miel";
+        String password = "meloMELO666";
+
+        Get get = new Get();
+
+        get.verLibros(user,password,url);
+
     }
 }
